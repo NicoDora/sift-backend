11c306e1a5b29310fd5cea708fe77cdea3ec5365
NicoDora <pjh76574734@gmail.com>
2025-12-18 15:05:24 +0900

    refactor/#7: 'setSwagger' 메서드 이름을 'setupSwagger'로 수정

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   });
   const bootstrapService = app.get<BootstrapService>(BootstrapService);
 
-  bootstrapService.setSwagger(app);
+  bootstrapService.setupSwagger(app);
 
   app.useLogger(app.get(Logger));
 
