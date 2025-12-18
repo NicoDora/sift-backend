a84bafcf94c0b14a5cc1aab036cf36d61999beb8
NicoDora <pjh76574734@gmail.com>
2025-12-18 14:20:20 +0900

    refactor/#7: BootstrapService를 추가하여 Swagger 설정 로직을 분리

---
+++ src/bootstrap.service.ts
@@ -0,0 +1,40 @@
+import { INestApplication, Injectable } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
+@Injectable()
+export class BootstrapService {
+  setSwagger(app: INestApplication) {
+    const config = new DocumentBuilder()
+      .setTitle("Sift API")
+      .setDescription("Sift 백엔드 API 문서입니다.")
+      .setVersion("1.0")
+      .addBearerAuth()
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("api-docs", app, document, {
+      swaggerOptions: {
+        persistAuthorization: true,
+        targetSorter: "alpha",
+        operationsSorter: (a: Map<any, any>, b: Map<any, any>) => {
+          const methodsOrder = {
+            post: "0",
+            get: "1",
+            put: "2",
+            patch: "3",
+            delete: "4",
+          };
+
+          return methodsOrder[a.get("method")].localeCompare(
+            methodsOrder[b.get("method")],
+          );
+        },
+      },
+    });
+  }
+
+  async startingServer(app: INestApplication) {
+    const port = process.env.PORT || 3000;
+
+    await app.listen(port);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,18 @@
 import { Logger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "@src/app.module";
+import { BootstrapService } from "@src/bootstrap.service";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true,
   });
+  const bootstrapService = app.get<BootstrapService>(BootstrapService);
+
+  bootstrapService.setSwagger(app);
 
   app.useLogger(app.get(Logger));
 
-  // Swagger 설정
-  const config = new DocumentBuilder()
-    .setTitle("Sift API")
-    .setDescription("Sift 백엔드 API 문서입니다.")
-    .setVersion("1.0")
-    .addBearerAuth()
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("api-docs", app, document, {
-    swaggerOptions: {
-      persistAuthorization: true,
-      targetSorter: "alpha",
-      operationsSorter: (a: Map<any, any>, b: Map<any, any>) => {
-        const methodsOrder = {
-          post: "0",
-          get: "1",
-          put: "2",
-          patch: "3",
-          delete: "4",
-        };
-
-        return methodsOrder[a.get("method")].localeCompare(
-          methodsOrder[b.get("method")],
-        );
-      },
-    },
-  });
-
-  await app.listen(process.env.PORT || 3000);
+  await bootstrapService.startingServer(app);
 }
 bootstrap();
