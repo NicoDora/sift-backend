7ca48e6b5372051eac2333b98f03a311a86fb4bb
NicoDora <pjh76574734@gmail.com>
2025-12-18 14:00:52 +0900

    setup/#7: swagger 설정
    - @nestjs/swagger 패키지 추가
    - swagger 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { Logger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "@src/app.module";
 
 async function bootstrap() {
@@ -8,6 +9,34 @@ async function bootstrap() {
   });
 
   app.useLogger(app.get(Logger));
+
+  // Swagger 설정
+  const config = new DocumentBuilder()
+    .setTitle("Sift API")
+    .setDescription("Sift 백엔드 API 문서입니다.")
+    .setVersion("1.0")
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api-docs", app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+      targetSorter: "alpha",
+      operationsSorter: (a: Map<any, any>, b: Map<any, any>) => {
+        const methodsOrder = {
+          post: "0",
+          get: "1",
+          put: "2",
+          patch: "3",
+          delete: "4",
+        };
+
+        return methodsOrder[a.get("method")].localeCompare(
+          methodsOrder[b.get("method")],
+        );
+      },
+    },
+  });
 
   await app.listen(process.env.PORT || 3000);
 }
