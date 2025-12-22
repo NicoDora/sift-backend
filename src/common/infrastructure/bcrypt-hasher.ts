d9486f6de1f183614af96bb3c236942b6b888a85
NicoDora <pjh76574734@gmail.com>
2025-12-22 17:01:47 +0900

    modify/#15: BcryptHasher 클래스에 @Injectable 데코레이터 추가

--- src/common/infrastructure/bcrypt-hasher.ts
+++ src/common/infrastructure/bcrypt-hasher.ts
@@ -1,8 +1,10 @@
+import { Injectable } from "@nestjs/common";
 import { IPasswordHasher } from "@src/modules/member/domain/interfaces/password-hasher.interface";
 import * as bcrypt from "bcrypt";
 
 const SALT_ROUNDS = 10;
 
+@Injectable()
 export class BcryptHasher implements IPasswordHasher {
   async hash(plainText: string): Promise<string> {
     return bcrypt.hash(plainText, SALT_ROUNDS);
