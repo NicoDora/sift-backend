6a8f62bb59c531fd3fbe658a8c0fcf45c7ac78ac
NicoDora <pjh76574734@gmail.com>
2025-12-26 16:27:28 +0900

    modify/#17: LoginRequestDto에서 이메일 값을 트리밍 하도록 개선

--- src/modules/auth/presentation/dtos/login-request.dto.ts
+++ src/modules/auth/presentation/dtos/login-request.dto.ts
@@ -1,8 +1,10 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { Transform } from "class-transformer";
 import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class LoginRequestDto {
   @ApiProperty({ example: "user@example.com", description: "이메일" })
+  @Transform(({ value }) => (typeof value === "string" ? value.trim() : value))
   @IsNotEmpty({ message: "이메일은 필수입니다." })
   @IsEmail({}, { message: "유효한 이메일 형식이 아닙니다." })
   readonly email: string;
