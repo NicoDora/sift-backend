889f0d805fed976f3da8a653ca213b14751b6e31
NicoDora <pjh76574734@gmail.com>
2025-12-26 13:27:02 +0900

    feat/#17: AppConfigService에 JWT 관련 설정 추가 및 JwtTokenService 구현

--- src/core/configs/app-config.service.ts
+++ src/core/configs/app-config.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
+import { StringValue } from "ms";
 
 const LOCAL = "local";
 const PRODUCTION = "production";
@@ -26,5 +27,21 @@ export class AppConfigService {
 
   get saltRounds(): number {
     return this.configService.get<number>("SALT_ROUNDS") || 10;
+  }
+
+  get jwtSecret(): string {
+    return (
+      this.configService.get<string>("JWT_SECRET_KEY") || "default-secret-key"
+    );
+  }
+
+  get jwtAccessExpiresIn(): StringValue {
+    return (this.configService.get<string>("JWT_ACCESS_EXPIRES_IN") ||
+      "1h") as StringValue;
+  }
+
+  get jwtRefreshExpiresIn(): StringValue {
+    return (this.configService.get<string>("JWT_REFRESH_EXPIRES_IN") ||
+      "7d") as StringValue;
   }
 }
---
+++ src/modules/user/infrastructure/adapters/jwt-token.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { AppConfigService } from "@src/core/configs/app-config.service";
+import { ITokenService } from "@src/modules/user/domain/service-interfaces/token-service.interface";
+
+@Injectable()
+export class JwtTokenService implements ITokenService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly appConfigService: AppConfigService,
+  ) {}
+
+  generateAccessToken(payload: any): string {
+    return this.jwtService.sign(payload, {
+      expiresIn: this.appConfigService.jwtAccessExpiresIn,
+    });
+  }
+
+  generateRefreshToken(payload: any): string {
+    return this.jwtService.sign(payload, {
+      expiresIn: this.appConfigService.jwtRefreshExpiresIn,
+    });
+  }
+
+  verifyToken(token: string): any {
+    return this.jwtService.verify(token);
+  }
+}
